28687440cafbd13e818cd7e52da453ec61e0092d
João Neto <164028729+JoaoNeto237@users.noreply.github.com>
2024-04-08 15:47:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python
+ Aplicacoes python
---
+++ tk.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+def btclick():
+    if(str(en.get()).isnumeric() and str(en2.get()).isnumeric()):
+        num1 = int(en.get())
+        num2 = int(en2.get())
+
+        lb["text"] = num1 + num2
+    else:
+        lb["text"] = "Valores informados invalidos"
+        
+janela = Tk()
+en = Entry(janela)
+en.place(x=100, y=100)
+
+en2 = Entry(janela)
+en2.place(x=100, y=150)
+
+bt = Button(janela, width=20, text="SOMAR", command= btclick)
+bt.place(x=100, y=200)
+
+lb = Label(janela, text=("Resultado"))
+lb.place(x=100, y=250)
+
+janela.geometry("300x300")
+janela.mainloop()
